21f565a7dd463d85898b18c4863fcc19e45e4cee
ghazibenrhouma <ghazybenrhouma@gmail.com>
2020-10-16 23:17:47 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+import UserList from "./containers/UserList/UserList";
+
+function App() {
+    return ( <
+        div className = "App" >
+        <
+        h1 > User List From Api < /h1> <UserList / >
+        <
+        /div>
+    );
+}
+
+export default App;
